3d6afc6b5c2677729ddd6d5a77c8d8b8d93d6a25
Nikita <48101012+ViperClaw1@users.noreply.github.com>
2024-01-16 12:35:45 +0400

    v0.9.1
    Fixed event on plus and minus buttons.

--- js/main.js
+++ js/main.js
@@ -2,34 +2,34 @@ main();
 
 function main () {
 
-    // const btnPlus = document.getElementById('plus');
-    // const btnMinus = document.getElementById('minus');
-    // const divNum = document.getElementById('number');
+    const btnPlus = document.getElementById('plus');
+    const btnMinus = document.getElementById('minus');
+    const divNum = document.getElementById('number');
 
     let currNum = parseInt(divNum.innerText); // parseInt конветирует строку в число
 
-    // btnPlus.onclick = function () {
-    //     currNum++;
-    //     divNum.innerText = currNum;
-    // }
-
-    // btnMinus.onclick = function () {
-    //     if (currNum > 1) currNum--;
-    //     divNum.innerText = currNum;
-    // }
-
-    document.
-        getElementById('minus').
-        addEventListener('click', () => {
-        if (currNum > 1) currNum--;
-        divNum.innerText = currNum;
-    })
-
-    document.
-        getElementById('plus').
-        addEventListener('click', () => {
+    btnPlus.onclick = function () {
         currNum++;
         divNum.innerText = currNum;
-    })
+    }
+
+    btnMinus.onclick = function () {
+        if (currNum > 1) currNum--;
+        divNum.innerText = currNum;
+    }
+
+    // document.
+    //     getElementById('minus').
+    //     addEventListener('click', () => {
+    //     if (currNum > 1) currNum--;
+    //     divNum.innerText = currNum;
+    // })
+
+    // document.
+    //     getElementById('plus').
+    //     addEventListener('click', () => {
+    //     currNum++;
+    //     divNum.innerText = currNum;
+    // })
 }
 
